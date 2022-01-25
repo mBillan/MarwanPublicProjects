641b7b0f33e33ad5cffd6bcfe97d1149cb55ef17
mBillan <49795811+mBillan@users.noreply.github.com>
2022-01-25 18:57:37 +0200

    Add midi utility file
    Reorganize the code to ease future enhancements.
    Add a utility file for midi stuff including notes, scales and notes range configurations

--- MidiMaker/random_midi_track.py
+++ MidiMaker/random_midi_track.py
@@ -7,46 +7,7 @@ Note: Make sure that you imported the package "mido"
 from __future__ import division
 import random
 from mido import Message, MidiFile, MidiTrack  #, MAX_PITCHWHEEL, bpm2tempo, tick2second
-from enum import Enum
-
-
-class Scale(Enum):
-    """
-    A representation of the musical scales.
-    Each scale is represented as a list of intervals from the base note.
-    """
-    MAJOR = [0, 2, 4, 5, 7, 9, 11, 12]
-    MINOR = [0, 2, 3, 5, 7, 8, 10, 12]
-
-
-class Note(Enum):
-    """
-    A representation of the middle flat notes.
-    """
-    A = 57
-    B = 59
-    C = 60  # middle C or C4
-    D = 62
-    E = 64
-    F = 65
-    G = 67
-
-
-def get_notes_range(key=Note.A.value, scale: Scale = Scale.MAJOR.value):
-    """
-    Gets a valid notes range based on the given key and scale
-
-    :param key: The key of the wanted notes
-    :param scale: The scale of the wanted notes
-    :return: A list of all the allowed notes.
-    """
-    notes = [key - 24 + dist for dist in scale] +\
-            [key - 12 + dist for dist in scale] +\
-            [key + dist for dist in scale] +\
-            [key + 12 + dist for dist in scale]
-    notes = list(dict.fromkeys(notes))
-
-    return notes
+from midi_utils import get_notes_range
 
 
 def generate_random_midi(number_of_beats=32, output_midi="musical_line.mid"):
